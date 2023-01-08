a9dbb28abe7ffcb75b879a67ef6a17405745752d
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-01-08 02:25:38 +0100

    Agregar recogida de datos al main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     private static void imprimirMensajeDeBienvenida() {
-        System.out.println("  ____                                         \n"
+        System.out.print("  ____                                         \n"
                 + "  |___ \\                                        \n"
                 + "    __) |    ___ _ __     _ __ __ _ _   _  __ _ \n"
                 + "   |__ <    / _ \\ '_ \\   | '__/ _` | | | |/ _` |\n"
@@ -11,7 +13,49 @@ public class Main {
 
     }
 
+    private static void imprimirMensajeDeSeleccionDeModo() {
+        System.out.println("Seleccione uno de los siguientes modos de juego:");
+        System.out.println("\t1. Jugador vs Jugador.");
+        System.out.println("\t2. Jugador vs IA.");
+        System.out.println("\t3. IA vs IA.");
+    }
+
+    private static void imprimirMensajeDeSeleccionDeTablero() {
+        System.out.print("Especifique la dimensión del tablero en el que se jugará (p. ej. 3 -> 3x3): ");
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         imprimirMensajeDeBienvenida();
+
+        // Obtener valores requeridos para empezar una partida de 3 en raya
+        while (true) {
+            // Obtener modo a jugar
+            imprimirMensajeDeSeleccionDeModo();
+            int modo = sc.nextInt();
+
+            // Comprobar valores inválidos para modo
+            if (modo < 1 || modo > 3) {
+                // Valor inválido, volver a pedir datos
+                System.out.println("Valor de modo incorrecto, recuerde que debe encontrarse entre 1 y 3...");
+                continue;
+            }
+
+            // Obtener dimensión del tablero
+            imprimirMensajeDeSeleccionDeTablero();
+            int dimension = sc.nextInt();
+
+            // Comprobar valores inválidos para dimensión
+            if (dimension < 3) {
+                System.out.println("Valor de dimensión incorrecto, recuerde que debe ser al menos 3...");
+                continue;
+            }
+
+            // Todos los valores son válidos, se sale del bucle
+            break;
+        }
+
+        sc.close();
     }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,26 +1,26 @@
 public class Tablero {
-    private int dimensionTablero;
+    private int dimension;
     /**
      * Matriz que representa las fichas introducidas en el tablero
      */
-    private int[][] fichasTablero;
+    private int[][] fichas;
 
     public Tablero(int dimensionTablero) {
         // Inicializar tablero
-        this.dimensionTablero = dimensionTablero;
-        this.fichasTablero = new int[this.dimensionTablero][this.dimensionTablero];
-        for (int i = 0; i < fichasTablero.length; i++) {
-            for (int j = 0; j < fichasTablero.length; j++) {
-                fichasTablero[i][j] = 0;
+        this.dimension = dimensionTablero;
+        this.fichas = new int[this.dimension][this.dimension];
+        for (int i = 0; i < fichas.length; i++) {
+            for (int j = 0; j < fichas.length; j++) {
+                fichas[i][j] = 0;
             }
         }
     }
 
     public void imprimirTablero() {
-        for (int i = 0; i < fichasTablero.length; i++) {
-            for (int j = 0; j < fichasTablero.length; j++) {
+        for (int i = 0; i < fichas.length; i++) {
+            for (int j = 0; j < fichas.length; j++) {
                 // Imprimir ficha actual
-                switch (fichasTablero[i][j]) {
+                switch (fichas[i][j]) {
                     case 1:
                         System.out.print("X");
                         break;
