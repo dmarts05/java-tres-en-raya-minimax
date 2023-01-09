a096c33bbb1ce5e62434c024822fd79c39f4a333
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-01-09 21:44:23 +0100

    Crear script para Replit

---
+++ replit-tres-en-raya.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Bienvenido al 3 en raya con IA Minimax, elige una opción:"
+echo
+echo "    1. Jugar al 3 en raya."
+echo "    2. Ejecutar pruebas de rendimiento de los algoritmos."
+echo "    3. Salir."
+
+read option
+
+case $option in
+    1)
+        # Ejecuta el fichero jar del juego
+        java -jar tres-en-raya.jar
+    ;;
+    
+    2)
+        # Ejecuta el fichero jar del juego
+        java -jar prueba-rendimiento.jar
+    ;;
+    
+    3)
+        echo "Saliendo..."
+    ;;
+    
+    *)echo "[ERROR] Opción no reconocida.";;
+esac
--- src-tres-en-raya.sh
+++ src-tres-en-raya.sh
@@ -4,7 +4,7 @@ echo "Bienvenido al 3 en raya con IA Minimax, elige una opción:"
 echo
 echo "    1. Jugar al 3 en raya."
 echo "    2. Ejecutar pruebas de rendimiento de los algoritmos."
-echo "    3. Salir"
+echo "    3. Salir."
 
 read option
 
@@ -23,5 +23,5 @@ case $option in
         echo "Saliendo..."
     ;;
     
-    *)echo "[ERROR] Opción no reconocida";;
+    *)echo "[ERROR] Opción no reconocida.";;
 esac
